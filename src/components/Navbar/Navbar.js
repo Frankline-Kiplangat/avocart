07971f68849bd3a2c1d69d0aee1aadccf1ebbf0b
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-09-01 17:13:38 +0300

    Update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,47 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
-const Navbar = ({ hamActive, setHamActive}) => {
-    const logo = 
-    "https://cdn.pixabay.com/photo/2016/06/06/12/45/avocado-1439329_1280.png";
-    const handleClick = () => {
-        setHamActive(!hamActive);
-};
+const Navbar = ({ hamActive, setHamActive }) => {
+  const logo =
+    "https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg";
+  const handleClick = () => {
+    setHamActive(!hamActive);
+  };
 
-return (
-    <nav className= {`${styles.navBarWrapper} center`}>
-        <div className= {`${styles.navBarWrapper} center`}>
-            <button
-            className={`${styles.humburger} ${hamActive && styles.active}`}
-            onClick={handleClick}
-            >
-                <span className={styles.humburgerLines}></span>
-            </button>
-            <div className={`{styles.navLeft}`}>
-                <img src={logo} alt="logo" className={styles.brand} />
-            </div>
-            <div className={`${styles.navRight} center`}>
-                <div className={styles.verticalWrapper}>
-                    <a href="/" className={`${styles.nav} center`}>
-                        Departments
-                    </a>
-                    <a href="/" className={`${styles.nav} center`}>
-                        Help
-                    </a>
-                    <a href="/" className={`${styles.nav} center`}>
-                        Log In
-                    </a>
-                    <button className={styles.signup}>Sign Up</button>
-                </div>
-            </div>
+  return (
+    <nav className={`${styles.navbarWrapper} center`}>
+      <div className={`${styles.navbarInner} center`}>
+        <button
+          className={`${styles.hamburger} ${hamActive && styles.active}`}
+          onClick={handleClick}
+        >
+          <span className={styles.hamburgerLines}></span>
+        </button>
+        <div className={`${styles.navLeft}`}>
+          <img src={logo} alt="logo" className={styles.brand} />
         </div>
+        <div className={`${styles.navRight} center`}>
+          <div className={styles.navLinksWrapper}>
+            <div className={styles.verticalLine}> </div>
+            <a href="/" className={`${styles.nav} center`}>
+              Departments
+            </a>
+            <a href="/" className={`${styles.nav} center`}>
+              More ways to shop
+            </a>
+            <a href="/" className={`${styles.nav} center`}>
+              Help
+            </a>
+          </div>
+          <div>
+            <a href="/" className={styles.login}>
+              Log in
+            </a>
+            <button className={styles.signup}>Sign Up</button>
+          </div>
+        </div>
+      </div>
     </nav>
-);
-
+  );
 };
 export default Navbar;
