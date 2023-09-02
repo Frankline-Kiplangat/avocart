4f85ee4daac3a5069940273722bf36db4b124a7d
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-09-02 12:33:52 +0300

    fix bugs

--- src/constants/growth.js
+++ src/constants/growth.js
@@ -1,6 +1,6 @@
 export const growth = [
   {
-    field: "500 million products",
+    field: "100k+ products",
     description: "available to shop across the catalog",
   },
   {
@@ -8,11 +8,11 @@ export const growth = [
     description: "from local grocers to chain stores",
   },
   {
-    field: "5,500+ cities",
-    description: "served across the U.S. & Canada",
+    field: "10+ cities",
+    description: "served across the Country",
   },
   {
-    field: "Millions of orders",
+    field: "Thousands of orders",
     description: "delivered or picked up yearly",
   },
 ];
