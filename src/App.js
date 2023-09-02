aa7a45e48c722ac9cc4226424970d6a8c3f1ef58
Frankline-Kiplangat <kipfrankline@gmail.com>
2023-09-02 11:19:21 +0300

    Add  FQA section and its components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar/Navbar";
 import NavbarResponsive from "./components/NavbarResponsive/NavbarResponsive";
 import Home from "./components/Home/Home";
 import Features from "./components/Features/Features";
+import Growth from "./components/Growth/Growth";
+import Questions from "./components/Questions/Questions";
+import Programs from "./components/Programs/Programs";
 
 const App = () =>{
   const [hamActive, setHamActive] = useState(false);
@@ -12,6 +15,11 @@ const App = () =>{
       <NavbarResponsive hamActive={hamActive} setHamActive={setHamActive}/>
       <Home />
       <Features />
+      <Growth />
+      <Questions />
+      <Programs programs={programs_user} />
+      <Programs programs={programs_shopper} />
+
     </div>
   );
 };
---
+++ src/constants/growth.js
@@ -0,0 +1,19 @@
+export const growth = [
+    {
+      field: "millions of products",
+      description: "available to shop across the catalog",
+    },
+    {
+      field: "5,000 stores",
+      description: "from local grocers to chain stores",
+    },
+    {
+      field: "10+ Towns",
+      description: "served countrywide",
+    },
+    {
+      field: "Millions of orders",
+      description: "delivered or picked up everyday",
+    },
+  ];
+  
